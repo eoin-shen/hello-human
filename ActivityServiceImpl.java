5cb443f05377dc22e3bbe9bc7f66fdcccf25eeb9
eoin-shen <eoin-shen@163.com>
2019-08-25 22:59:09 +0800

    新增测试文件

---
+++ ActivityServiceImpl.java
@@ -0,0 +1,28 @@
+public class MultiUploadAction extends ActionSupport {
+// 接收多文件上传参数，提供数组接收就可以了
+private File[] upload;
+private String[] uploadContentType;
+private String[] uploadFileName;
+public void setUpload(File[] upload) {
+this.upload = upload;
+}
+public void setUploadContentType(String[] uploadContentType) {
+this.uploadContentType = uploadContentType;
+}
+public void setUploadFileName(String[] uploadFileName) {
+this.uploadFileName = uploadFileName;
+}
+@Override
+public String execute() throws Exception {
+for (int i = 0; i < upload.length; i++) {
+// 循环完成上传
+File srcFile = upload[i];
+String filename = uploadFileName[i];
+// 定义目标文件
+File destFile = new File(ServletActionContext.getServletContext()
+.getRealPath("/upload" + "/" + filename));
+FileUtils.copyFile(srcFile, destFile);
+}
+return NONE;
+}
+}
